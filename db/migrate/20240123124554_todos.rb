a13c41e66c3994006208957d1254b8c0ff8afba2
Olive Nives <olivenives63@gmail.com>
2024-01-23 15:57:59 +0300

    add model

---
+++ db/migrate/20240123124554_todos.rb
@@ -0,0 +1,6 @@
+class Todos < ActiveRecord::Migration[7.1]
+  def change
+    create_table :todos
+     
+  end
+end
--- spec/requests/todos_requests_spec.rb
+++ spec/requests/todos_requests_spec.rb
@@ -6,6 +6,10 @@ RSpec.describe "Todos", type: :request do
             get '/todos'
             expect(response).to have_http_status(:success)
         end
-        
+        it 'should return list of todos' do 
+            todo = Todo.create
+            get '/todos'
+            expect(assigns(:todos)).to include(todo)
+        end
     end
 end
