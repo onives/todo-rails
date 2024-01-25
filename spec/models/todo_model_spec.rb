fb57eb46b2d6a3eb230d4440dabd97155e91a966
Olive Nives <olivenives63@gmail.com>
2024-01-25 17:05:19 +0300

    Require model to have a title

--- spec/models/todo_model_spec.rb
+++ spec/models/todo_model_spec.rb
@@ -4,7 +4,12 @@ require 'rails_helper'
 
 RSpec.describe Todo, type: :model do
   it 'should save a todo' do
-    pending 'Working on outer loop test in the controller, first'
-    Todo.new('a title', ' a body')
+    subject = Todo.new(body: 'a body', title: 'a title')
+    expect(subject).to be_valid
+  end
+
+  it 'should a title' do
+    subject = Todo.new(body: 'a body')
+    expect(subject).not_to be_valid
   end
 end
