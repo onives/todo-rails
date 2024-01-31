cd08148dffd9a98fb353a32ecebc0be5ae0c8d1b
Olive Nives <olivenives63@gmail.com>
2024-01-31 12:33:33 +0300

    rubocop fixes

--- db/migrate/20240123124554_todos.rb
+++ db/migrate/20240123124554_todos.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
+# some comment here
 class Todos < ActiveRecord::Migration[7.1]
   def change
-    create_table :todos
+    create_table :todos # rubocop:disable Rails/CreateTableWithTimestamps
   end
 end
--- db/migrate/20240124130012_add_columns_to_todos.rb
+++ db/migrate/20240124130012_add_columns_to_todos.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# some comment here
 class AddColumnsToTodos < ActiveRecord::Migration[7.1]
   def change
     add_column :todos, :title, :string
